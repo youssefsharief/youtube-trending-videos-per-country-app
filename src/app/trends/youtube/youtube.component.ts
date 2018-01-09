3e95f74dc5b1ce0949fbf921e98086bfbcd2003f
Youssef Sherif <sharief@aucegypt.edu>
2018-01-09 08:38:37 +0200

    Add comment

--- src/app/trends/youtube/youtube.component.ts
+++ src/app/trends/youtube/youtube.component.ts
@@ -50,6 +50,7 @@ export class YoutubeComponent implements OnInit {
             this.nextPageToken = result.nextPageToken
             // Concat videos to current videos so that we do not lose previously fetched videos in case this is not the first batch
             this.trendingVideos = this.trendingVideos.concat(result.items.map(item => {
+                // Only include undeleted YouTube videos by checking that the snippet property exists
                 if (item.snippet)
                     return {
                         id: item.id,
